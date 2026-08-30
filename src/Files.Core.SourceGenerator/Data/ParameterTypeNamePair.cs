95fb47d6a58c8e3c9f0cf5bd71adda7f4c331837
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 22:00:38 +0100

    Remove copyright comments from ParameterTypeNamePair.cs
    Removed copyright and license comments from the file.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.Core.SourceGenerator/Data/ParameterTypeNamePair.cs
+++ src/Files.Core.SourceGenerator/Data/ParameterTypeNamePair.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) Files Community
-// SPDX-License-Identifier: MPL-2.0
-
+﻿
 namespace Files.Core.SourceGenerator.Data
 {
 	internal record ParameterTypeNamePair(string FullyQualifiedTypeName, string ValueName);
