6ce2043eb9037fb70563f98b098edd5b4b6caee5
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 23:13:46 +0100

    Remove copyright and license comments
    Removed copyright and license comments from CompressIntoArchiveAction.cs.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Actions/Content/Archives/Compress/CompressIntoArchiveAction.cs
+++ src/Files.App/Actions/Content/Archives/Compress/CompressIntoArchiveAction.cs
@@ -1,8 +1,6 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
-
+﻿
 using Files.App.Dialogs;
-using Microsoft.UI.Xaml.Controls;
+using Unity-Billal-mesloub.UI.Xaml.Controls;
 using Windows.Foundation.Metadata;
 
 namespace Files.App.Actions
