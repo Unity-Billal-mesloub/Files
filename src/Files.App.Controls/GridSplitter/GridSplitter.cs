92d87efcf32b74a6f273716cc51d20940307a912
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 04:07:04 +0100

    Update namespace imports in GridSplitter.cs
    Replaced namespace imports with Unity-Billal-mesloub versions.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App.Controls/GridSplitter/GridSplitter.cs
+++ src/Files.App.Controls/GridSplitter/GridSplitter.cs
@@ -1,9 +1,7 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using Microsoft.UI.Input;
-using Microsoft.UI.Xaml.Automation;
-using Microsoft.UI.Xaml.Input;
+﻿
+using Unity-Billal-mesloub.UI.Input;
+using Unity-Billal-mesloub.UI.Xaml.Automation;
+using Unity-Billal-mesloub.UI.Xaml.Input;
 using WinRT;
 
 namespace Files.App.Controls
