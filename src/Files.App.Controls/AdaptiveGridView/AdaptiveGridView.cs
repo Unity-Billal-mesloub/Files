36866b64492b6ad8d7c4e8a6c5af7af829ec823e
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 03:23:13 +0100

    Remove license comments from AdaptiveGridView.cs
    Removed licensing comments from the top of the file.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App.Controls/AdaptiveGridView/AdaptiveGridView.cs
+++ src/Files.App.Controls/AdaptiveGridView/AdaptiveGridView.cs
@@ -1,6 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
+﻿
 using Microsoft.UI.Xaml.Data;
 using Windows.Foundation.Collections;
 using WinRT;
