d1200fccac71567bc9a5b1fef921dc985d7ceb26
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 03:38:30 +0100

    Update namespaces in BladeItemAutomationPeer.cs
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App.Controls/BladeView/BladeItemAutomationPeer.cs
+++ src/Files.App.Controls/BladeView/BladeItemAutomationPeer.cs
@@ -1,9 +1,7 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
 
-using CommunityToolkit.WinUI;
-using Microsoft.UI.Xaml.Automation;
-using Microsoft.UI.Xaml.Automation.Peers;
+using Unity-Billal-mesloubToolkit.WinUI;
+using Unity-Billal-mesloub.UI.Xaml.Automation;
+using Unity-Billal-mesloub.UI.Xaml.Automation.Peers;
 
 namespace Files.App.Controls
 {
