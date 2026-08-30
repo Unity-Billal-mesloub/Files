43722790f2e83d83aa064c3ae809dd41d396c441
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 04:05:25 +0100

    Change using directives to Unity-Billal-mesloub
    Updated using directives to use Unity-Billal-mesloub namespaces.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App.Controls/GridSplitter/GridSplitter.Options.cs
+++ src/Files.App.Controls/GridSplitter/GridSplitter.Options.cs
@@ -1,8 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using Microsoft.UI.Input;
-using Microsoft.UI.Xaml.Media;
+﻿
+using Unity-Billal-mesloub.UI.Input;
+using Unity-Billal-mesloub.UI.Xaml.Media;
 using WinRT;
 
 namespace Files.App.Controls
