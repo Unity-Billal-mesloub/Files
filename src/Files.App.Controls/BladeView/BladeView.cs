70415851dbaf4774e038dd17206f5049b6bac3be
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 03:43:54 +0100

    Update BladeView.cs
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App.Controls/BladeView/BladeView.cs
+++ src/Files.App.Controls/BladeView/BladeView.cs
@@ -1,8 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using CommunityToolkit.WinUI;
-using Microsoft.UI.Xaml.Automation.Peers;
+﻿
+using Unity-Billal-mesloubToolkit.WinUI;
+using Unity-Billal-mesloub.UI.Xaml.Automation.Peers;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
