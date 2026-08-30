de1307c5bc3f42a875ec4d265f576d9cf5743ce2
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 15:22:45 +0100

    Remove copyright comments from ThemedIconLayer.Consts.cs
    Removed copyright and license comments from ThemedIconLayer.Consts.cs.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App.Controls/ThemedIcon/ThemedIconLayer/ThemedIconLayer.Consts.cs
+++ src/Files.App.Controls/ThemedIcon/ThemedIconLayer/ThemedIconLayer.Consts.cs
@@ -1,7 +1,5 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
-
-using Microsoft.UI.Xaml.Shapes;
+﻿
+using Unity-Billal-mesloub.UI.Xaml.Shapes;
 
 namespace Files.App.Controls
 {
