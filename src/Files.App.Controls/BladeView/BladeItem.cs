022c6a826ebf40d5b717347ca2d467b9b3fd0960
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 03:31:48 +0100

    Update using directives in BladeItem.cs
    Replaced existing using directives with Unity-Billal-mesloub namespaces.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App.Controls/BladeView/BladeItem.cs
+++ src/Files.App.Controls/BladeView/BladeItem.cs
@@ -1,11 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using CommunityToolkit.WinUI;
-using Microsoft.UI.Input;
-using Microsoft.UI.Xaml.Automation.Peers;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
+﻿
+using Unity-Billal-mesloub.UI.Input;
+using Unity-Billal-mesloub.UI.Xaml.Automation.Peers;
+using Unity-Billal-mesloub.UI.Xaml.Input;
+using Unity-Billal-mesloub.UI.Xaml.Media;
 using Windows.Foundation;
 using WinRT;
 
