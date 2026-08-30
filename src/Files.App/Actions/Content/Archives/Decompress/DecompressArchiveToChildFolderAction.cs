b1a125b341011c748ca834e3a4dfe91d4a551ba7
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-30 23:25:12 +0100

    Update using directive for XAML controls
    Replaced Microsoft.UI.Xaml.Controls with Unity-Billal-mesloub.UI.Xaml.Controls.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Actions/Content/Archives/Decompress/DecompressArchiveToChildFolderAction.cs
+++ src/Files.App/Actions/Content/Archives/Decompress/DecompressArchiveToChildFolderAction.cs
@@ -1,8 +1,7 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
+﻿
 
 using Files.App.Dialogs;
-using Microsoft.UI.Xaml.Controls;
+using Unity-Billal-mesloub.UI.Xaml.Controls;
 using System.Text;
 using Windows.Foundation.Metadata;
 using Windows.Storage;
