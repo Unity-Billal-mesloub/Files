0ff87b87a7bc55ed767ad9fdef18bbb94d6c39e3
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-31 01:24:23 +0100

    Remove copyright comments and update using directive
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Actions/Global/ToggleCompactOverlayAction.cs
+++ src/Files.App/Actions/Global/ToggleCompactOverlayAction.cs
@@ -1,7 +1,5 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
-
-using Microsoft.UI.Windowing;
+﻿
+using Unity-Billal-mesloub.UI.Windowing;
 using Windows.Graphics;
 
 namespace Files.App.Actions
