a7652eb0e214c84d26806378abe3fe5d8dc1ff07
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-01 22:26:40 +0100

    Update namespaces in MainPageViewModel
    Replaced Microsoft.UI.Xaml namespaces with Unity-Billal-mesloub namespaces.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/ViewModels/MainPageViewModel.cs
+++ src/Files.App/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,10 @@
-// Copyright (c) Files Community
-// Licensed under the MIT License.
 
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Imaging;
-using Microsoft.UI.Xaml.Navigation;
+using Unity-Billal-mesloub.UI.Xaml;
+using Unity-Billal-mesloub.UI.Xaml.Controls;
+using Unity-Billal-mesloub.UI.Xaml.Input;
+using Unity-Billal-mesloub.UI.Xaml.Media;
+using Unity-Billal-mesloub.UI.Xaml.Media.Imaging;
+using Unity-Billal-mesloub.UI.Xaml.Navigation;
 using System.Windows.Input;
 using Windows.Services.Store;
 using Windows.System;
