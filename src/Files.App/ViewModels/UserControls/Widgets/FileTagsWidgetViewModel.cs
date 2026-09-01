7e29271f9363267c8c43c1af8fb16b11030036b0
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-01 21:42:00 +0100

    Update FileTagsWidgetViewModel.cs
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/ViewModels/UserControls/Widgets/FileTagsWidgetViewModel.cs
+++ src/Files.App/ViewModels/UserControls/Widgets/FileTagsWidgetViewModel.cs
@@ -1,7 +1,5 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
-
-using Microsoft.UI.Xaml.Controls;
+﻿
+using Unity-Billal-mesloub.UI.Xaml.Controls;
 using System.IO;
 using Windows.Storage;
 
