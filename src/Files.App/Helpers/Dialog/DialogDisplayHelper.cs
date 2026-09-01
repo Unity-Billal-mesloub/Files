b3a92091e97eb1e6ccebb5f072da3f2830686e16
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-01 22:45:28 +0100

    Change using directive for Microsoft UI controls
    Updated using directive for UI controls.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Helpers/Dialog/DialogDisplayHelper.cs
+++ src/Files.App/Helpers/Dialog/DialogDisplayHelper.cs
@@ -1,8 +1,6 @@
-// Copyright (c) Files Community
-// Licensed under the MIT License.
 
 using Files.App.Dialogs;
-using Microsoft.UI.Xaml.Controls;
+using Unity-Billal-mesloub.UI.Xaml.Controls;
 using WinRT;
 
 namespace Files.App.Helpers
