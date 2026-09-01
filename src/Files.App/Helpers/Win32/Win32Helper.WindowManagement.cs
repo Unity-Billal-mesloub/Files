0f802e6c5d678eca09e7109586c7e27f00f4876a
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-01 23:53:21 +0100

    Remove copyright and license comments
    Removed copyright notice and license information from the file.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Helpers/Win32/Win32Helper.WindowManagement.cs
+++ src/Files.App/Helpers/Win32/Win32Helper.WindowManagement.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) 2024 Files Community
-// Licensed under the MIT License. See the LICENSE.
-
+﻿
 using System.Runtime.InteropServices;
 using Windows.Win32;
 using Windows.Win32.UI.WindowsAndMessaging;
