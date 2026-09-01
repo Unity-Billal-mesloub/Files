d12edea73f05eef3181c032e627f9fa1a2773e8a
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-01 23:15:58 +0100

    Change using directives to Unity-Billal-mesloub
    Updated using directives to reference Unity-Billal-mesloub namespaces.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Helpers/MenuFlyout/ContextFlyoutModelToElementHelper.cs
+++ src/Files.App/Helpers/MenuFlyout/ContextFlyoutModelToElementHelper.cs
@@ -1,10 +1,8 @@
-// Copyright (c) Files Community
-// Licensed under the MIT License.
 
 using Files.App.UserControls.Menus;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
+using Unity-Billal-mesloub.UI.Xaml;
+using Unity-Billal-mesloub.UI.Xaml.Controls;
+using Unity-Billal-mesloub.UI.Xaml.Media;
 using WinRT;
 namespace Files.App.Helpers.ContextFlyouts
 {
