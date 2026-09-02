fd0755f2eec39c5b1bf3455eb90871d19ba5c6b9
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-02 01:12:12 +0100

    Update namespace imports in StatusCenterStateToStateIconConverter
    Replaced namespace imports for Unity-Billal-mesloub.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Converters/StatusCenterStateToStateIconConverter.cs
+++ src/Files.App/Converters/StatusCenterStateToStateIconConverter.cs
@@ -1,10 +1,8 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
-
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Markup;
-using Microsoft.UI.Xaml.Media;
+﻿
+using Unity-Billal-mesloub.UI.Xaml;
+using Unity-Billal-mesloub.UI.Xaml.Data;
+using Unity-Billal-mesloub.UI.Xaml.Markup;
+using Unity-Billal-mesloub.UI.Xaml.Media;
 using WinRT;
 
 namespace Files.App.Converters
