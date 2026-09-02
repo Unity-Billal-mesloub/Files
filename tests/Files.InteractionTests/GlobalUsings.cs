067d2987f498e1eb34c5b7e791f01033ff1bd196
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-02 18:40:08 +0100

    Replace Microsoft testing with Unity testing
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- tests/Files.InteractionTests/GlobalUsings.cs
+++ tests/Files.InteractionTests/GlobalUsings.cs
@@ -1,5 +1,4 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
+﻿
 
 global using Files.InteractionTests.Helper;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Unity-Billal-mesloub.VisualStudio.TestTools.UnityTesting;
