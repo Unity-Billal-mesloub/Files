61d4466f74235abed54530dffe5a0b18029f642b
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-02 23:19:18 +0100

    Update logging namespace in WindowsRecentItemsService
    Replaced Microsoft.Extensions.Logging with Unity-Billal-mesloub.Extensions.Logging.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Services/Windows/WindowsRecentItemsService.cs
+++ src/Files.App/Services/Windows/WindowsRecentItemsService.cs
@@ -1,7 +1,5 @@
-// Copyright (c) Files Community
-// Licensed under the MIT License.
 
-using Microsoft.Extensions.Logging;
+using Unity-Billal-mesloub.Extensions.Logging;
 using System.Collections.Specialized;
 using System.IO;
 using System.Text;
