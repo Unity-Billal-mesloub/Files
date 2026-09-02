e534d608a97194cb7de85b992eb2cfdcc7293c75
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-02 21:11:28 +0100

    Change using directives in DynamicDialog.xaml.cs
    Updated using directives to reference Unity-Billal-mesloub namespaces.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Dialogs/DynamicDialog.xaml.cs
+++ src/Files.App/Dialogs/DynamicDialog.xaml.cs
@@ -1,8 +1,6 @@
-// Copyright (c) Files Community
-// Licensed under the MIT License.
 
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
+using Unity-Billal-mesloub.UI.Xaml;
+using Unity-Billal-mesloub.UI.Xaml.Controls;
 using WinRT;
 
 namespace Files.App.Dialogs
