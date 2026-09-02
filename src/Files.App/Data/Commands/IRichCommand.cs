89b3ed707ca38f25a424ea990b976778f312d5fd
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-02 01:44:15 +0100

    Refactor IRichCommand.cs to update namespaces
    Removed copyright notice and updated using directives.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- src/Files.App/Data/Commands/IRichCommand.cs
+++ src/Files.App/Data/Commands/IRichCommand.cs
@@ -1,10 +1,8 @@
-﻿// Copyright (c) Files Community
-// Licensed under the MIT License.
-
+﻿
 using Files.App.Controls;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
+using Unity-Billal-mesloub.UI.Xaml;
+using Unity-Billal-mesloub.UI.Xaml.Controls;
+using Unity-Billal-mesloub.UI.Xaml.Input;
 using System.Windows.Input;
 
 namespace Files.App.Data.Commands
